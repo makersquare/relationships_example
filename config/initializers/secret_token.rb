f40e91cccf1eb0d32e11eecedf67f19335471255
Michael Ornellas <ornellasm@gmail.com>
2014-03-25 10:50:24 -0500

    initial commit with user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yourownstagram::Application.config.secret_key_base = 'e56935133da7fc9dafd567b2ec3d74d7310069850c8fb7423ca8591f009577f688eb07d7548bb4ed0668bf24ef3fd5d65af6b6af6216e8d9934235fa9a9e071f'
