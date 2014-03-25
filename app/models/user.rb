3581ecb97667daaef7a4e7a6df54c45ef92d200c
Michael Ornellas <ornellasm@gmail.com>
2014-03-25 11:09:12 -0500

    add relationship model, relationship and reverse_relationship associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  # the join table to track followers and followed users
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  # find users the current user has followed
+  has_many :followed_users, through: :relationships, source: :followed
+
+  # look at the join table from the POV of who is following the current user
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship"
+  # find other users who have followed the current user
+  has_many :followers, through: :reverse_relationships, source: :follower
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325154737) do
+ActiveRecord::Schema.define(version: 20140325155030) do
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
